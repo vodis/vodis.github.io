56477dd8c935e28d62631559ab399de718fbe61c
vodis <i40217891@gmail.com>
2018-12-07 10:51:05 +0200

    add TaskForm

---
+++ src/Components/TasksList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AddTask extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <h1>Items</h1>
+                    <input/>
+                    <button>Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddTask;
