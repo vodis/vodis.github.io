510dc44ff741dfb07b90397c8f43228ceda125cf
vodis <i40217891@gmail.com>
2018-12-08 11:19:15 +0200

    reorganisation components

---
+++ src/Components/TasksForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Tasks from "./Tasks";
+
+class TasksForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            values: []
+        };
+        this.updateValue = this.updateValue.bind(this);
+        this.submitValue = this.submitValue.bind(this);
+        this.echoTask = this.echoTask.bind(this);
+    }
+
+    updateValue(e) {
+        this.setState({
+            values: e.target.value
+        });
+    }
+
+    submitValue(e) {
+        e.preventDefault();
+        var item = this.refs.item.value;
+        this.setState({
+            values: this.state.values.concat(item)
+        });
+        this.refs.item.value = "";
+        this.echoTask(item);
+    }
+
+    echoTask(item) {
+        return (
+            <Tasks value={item}/>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Items</h2>
+                <form onSubmit={this.submitValue}>
+                    <input
+                        type="text"
+                        placeholder="Type name here..."
+                        value={this.state.value}
+                        onChange={this.updateValue}
+                        ref="item"
+                    />
+                    <button type="summit">Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default TasksForm;
