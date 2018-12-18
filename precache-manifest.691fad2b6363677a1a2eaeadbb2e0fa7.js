a4a79f5159c9c18dd7b63bca586bd31678a81408
vodis <i40217891@gmail.com>
2018-12-18 03:14:22 +0200

    Updates

---
+++ precache-manifest.691fad2b6363677a1a2eaeadbb2e0fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97ecdce2aa0a367dadfd",
+    "url": "/vodis.github.io/static/js/runtime~main.97ecdce2.js"
+  },
+  {
+    "revision": "95284e1812897dfb1540",
+    "url": "/vodis.github.io/static/js/main.95284e18.chunk.js"
+  },
+  {
+    "revision": "9781b06c7d1807651464",
+    "url": "/vodis.github.io/static/js/1.9781b06c.chunk.js"
+  },
+  {
+    "revision": "95284e1812897dfb1540",
+    "url": "/vodis.github.io/static/css/main.888fa1fa.chunk.css"
+  },
+  {
+    "revision": "e9aa450bc7b3097c2b099fafeb1cdf41",
+    "url": "/vodis.github.io/index.html"
+  }
+];
