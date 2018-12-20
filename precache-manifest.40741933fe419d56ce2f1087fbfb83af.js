314da1b953f45d03e3e29de1ddd4e906ef9bb40f
vodis <i40217891@gmail.com>
2018-12-20 02:08:56 +0200

    New state

---
+++ precache-manifest.40741933fe419d56ce2f1087fbfb83af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b195f03c8369b94341c",
+    "url": "/static/js/main.2b195f03.chunk.js"
+  },
+  {
+    "revision": "9781b06c7d1807651464",
+    "url": "/static/js/1.9781b06c.chunk.js"
+  },
+  {
+    "revision": "2b195f03c8369b94341c",
+    "url": "/static/css/main.888fa1fa.chunk.css"
+  },
+  {
+    "revision": "56ead394f8d9921105689354704097dc",
+    "url": "/index.html"
+  }
+];
